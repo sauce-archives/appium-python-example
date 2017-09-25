90809a35d674b4de24e815121964afebaa732921
abdullahawara <abdulla@Abdulla-Hawara-NAT.local>
2017-09-25 14:46:48 +0200

    fix envs

--- appium_basic_test.py
+++ appium_basic_test.py
@@ -7,11 +7,11 @@ class TestClass(unittest.TestCase):
     def setUp(self):
         desired_caps = {}
         desired_caps['testobject_api_key'] = os.environ['TESTOBJECT_API_KEY']
-        desired_caps['testobject_device'] = os.getenv['TESTOBJECT_DEVICE']
-        desired_caps['deviceName'] = os.getenv('DEVICE_NAME')
+        desired_caps['testobject_device'] = os.getenv['TESTOBJECT_DEVICE', '']
+        desired_caps['deviceName'] = os.getenv('DEVICE_NAME', '')
         desired_caps['testobject_appium_version'] = os.getenv('APPIUM_VERSION', '1.6.4')
-        desired_caps['TESTOBJECT_SESSION_CREATION_RETRY'] = os.getenv('TESTOBJECT_SESSION_CREATION_RETRY')
-        desired_caps['TESTOBJECT_SESSION_CREATION_TIMEOUT'] = os.getenv('TESTOBJECT_SESSION_CREATION_TIMEOUT')
+        desired_caps['TESTOBJECT_SESSION_CREATION_RETRY'] = os.getenv('TESTOBJECT_SESSION_CREATION_RETRY', '')
+        desired_caps['TESTOBJECT_SESSION_CREATION_TIMEOUT'] = os.getenv('TESTOBJECT_SESSION_CREATION_TIMEOUT', '')
         testobject_endpoint = os.getenv('APPIUM_URL', 'http://appium.testobject.com/api/appium/wd/hub')
         self.driver = webdriver.Remote(testobject_endpoint, desired_caps)
 
